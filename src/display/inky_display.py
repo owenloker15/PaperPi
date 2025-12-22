c8142bd94a8a65e66d0791070b518933221bebbf
Owen Loker <owenloker15@gmail.com>
2025-12-22 11:17:29 -0500

    try and add mutexs

--- src/display/inky_display.py
+++ src/display/inky_display.py
@@ -1,4 +1,9 @@
+import threading
+
 from display.idisplay import IDisplay
+
+mutex = threading.Lock()
+
 
 class InkyDisplay(IDisplay):
     def __init__(self, device_config):
@@ -7,6 +12,7 @@ class InkyDisplay(IDisplay):
         print("Created inky display")
         try:
             from inky.auto import auto
+
             self.inky = auto()
         except (ImportError, RuntimeError):
             print("Inky not found!")
@@ -16,11 +22,13 @@ class InkyDisplay(IDisplay):
             print("Inky was not found!")
             return
 
-        resoulution = self.device_config['resolution']
-        img = img.convert("RGB").resize((resoulution['width'], resoulution['height']))
+        resoulution = self.device_config["resolution"]
+        img = img.convert("RGB").resize((resoulution["width"], resoulution["height"]))
         img.load()
 
-        self.inky.set_image(img)
-        print("Uploading image!")
-        self.inky.show()
-        print("Image uploaded!")
+        with mutex:
+            self.inky.set_image(img)
+            print("Uploading image!")
+            self.inky.show()
+            print("Image uploaded!")
+
--- src/display/mock_display.py
+++ src/display/mock_display.py
@@ -1,8 +1,17 @@
+import threading
+
 from display.idisplay import IDisplay
+
+mutex = threading.Lock()
+
 
 class MockDisplay(IDisplay):
     def __init__(self, device_config):
         super().__init__(device_config)
 
     def upload_image(self, img):
-        img.show()
+        with mutex:
+            print("Showing image!")
+            img.show()
+            print("Image shown!")
+
